bd1a0727401e6081707d5801205a7728924255ed
anan109019234 <9fl0ijn5@gmail.com>
2024-07-25 15:00:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,23 @@ def initialize_session_state():
         st.session_state['url_history'] = load_url_history()
 
 def welcome_page():
+    st.title("Selamat Datang di Aplikasi Deteksi Phishing")
+    st.markdown("""
+        **Tentang Aplikasi Ini:**
+        Aplikasi ini dirancang untuk membantu Anda mengidentifikasi apakah sebuah URL aman untuk dikunjungi atau berpotensi menjadi phishing. Phishing adalah teknik penipuan yang digunakan oleh pelaku kejahatan siber untuk mencuri informasi pribadi seperti kata sandi, informasi keuangan, dan data sensitif lainnya dengan menipu pengguna agar mengunjungi situs web palsu yang terlihat sah.
+
+        **Tujuan Aplikasi:**
+        Tujuan utama aplikasi ini adalah memberikan alat yang sederhana namun efektif untuk memeriksa keamanan URL secara real-time. Dengan menggunakan algoritma machine learning seperti Random Forest, aplikasi ini menganalisis berbagai fitur dari URL dan memberikan penilaian apakah URL tersebut berpotensi berbahaya atau tidak.
+
+        **Untuk Siapa Aplikasi Ini:**
+        Aplikasi ini dirancang untuk semua pengguna yang ingin melindungi diri mereka dari serangan phishing, termasuk individu yang menggunakan internet untuk aktivitas sehari-hari maupun profesional yang mengelola data sensitif.
+
+        **Landasan Utama:**
+        Landasan utama pembuatan aplikasi ini adalah untuk memberikan solusi praktis dan mudah diakses dalam melawan ancaman phishing yang semakin canggih. Dengan memanfaatkan teknologi machine learning dan analisis fitur URL, aplikasi ini bertujuan untuk meningkatkan kesadaran dan keamanan pengguna di dunia maya.
+
+        **Tujuan Pembuatan:**
+        Tujuan pembuatan aplikasi ini adalah untuk memberikan alat yang efisien dalam mendeteksi dan memitigasi risiko phishing, serta memberikan edukasi kepada pengguna tentang tanda-tanda phishing dan praktik terbaik untuk menjaga keamanan informasi pribadi mereka.
+    """)
     st.title("Yohohohoho!")
     st.image("assets/heker.gif", use_column_width=True)
     st.markdown("""
@@ -151,11 +168,11 @@ def main():
     initialize_session_state()
 
     selected = option_menu(
-        menu_title=None,  
-        options=["Selamat Datang", "Panduan Aplikasi", "Periksa Disini", "Daftar URL", "Tentang Saya"],  
-        icons=["house", "book", "search", "list", "envelope"],  
-        menu_icon="cast",  
-        default_index=0,  
+        menu_title=None,
+        options=["Selamat Datang", "Panduan Aplikasi", "Periksa Disini", "Daftar URL", "Tentang Saya"],
+        icons=["house", "book", "search", "list", "envelope"],
+        menu_icon="cast",
+        default_index=0,
         orientation="horizontal",
     )
 
