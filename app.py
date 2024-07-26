370e10bedf7981ff5b726fee707ac972dee69973
anan109019234 <9fl0ijn5@gmail.com>
2024-07-26 12:34:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def welcome_page():
         st.write("")
 
     with col2:
-        st.image("assets/heker.gif", width=500)
+        st.image("assets/heker.gif", width=1000)
 
     with col3:
         st.write("")
